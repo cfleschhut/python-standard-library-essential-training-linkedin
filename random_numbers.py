188ada69184e89ee8c9b969cdec80208df6eae9a
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-11-08 00:51:41 +0100

    Random numbers

---
+++ random_numbers.py
@@ -0,0 +1,16 @@
+import random
+
+for i in range(5):
+    print(f"{'heads' if random.random() <= 0.5 else 'tails'}")
+
+print(random.uniform(1, 100))
+print(random.randint(1, 100))
+print(random.randrange(0, 101, 5))
+
+random.seed(1)
+print(random.random())
+print(random.random())
+
+random.seed(1)
+print(random.random())
+print(random.random())

256dea99874c9f48077317e2220b6db77e35ef5f
Christian Fleschhut <weltrekordhalter@gmail.com>
2022-11-12 20:25:38 +0100

    Add descriptive fn names

--- working_with_numbers/01_random_numbers.py
+++ working_with_numbers/01_random_numbers.py
@@ -3,19 +3,35 @@ import random
 
 def coin_toss():
     for i in range(5):
-        print(f"{'heads' if random.random() <= 0.5 else 'tails'}")
+        print(f"{'Heads' if random.random() <= 0.5 else 'Tails'}")
 
-# print(random.uniform(1, 100))
-# print(random.randint(1, 100))
-# print(random.randrange(0, 101, 5))
 
-# random.seed(1)
-# print(random.random())
-# print(random.random())
+def random_num_within_range():
+    print(random.uniform(1, 10))
 
-# random.seed(1)
-# print(random.random())
-# print(random.random())
+
+def random_int_within_range_inclusive():
+    print(random.randint(1, 10))
+
+
+def random_int_within_stepped_range():
+    print(random.randrange(0, 11, 5))
+
+
+def position_generator():
+    random.seed(1)
+
+    print(random.random())
+    print(random.random())
 
 
 coin_toss()
+
+random_num_within_range()
+
+random_int_within_range_inclusive()
+
+random_int_within_stepped_range()
+
+position_generator()
+position_generator()
